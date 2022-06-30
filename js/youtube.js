da5738bca2e6b6fb4aa9537bad437eecf57c41bf
만두아빠 <86029972+mandoopapa@users.noreply.github.com>
2022-06-30 10:09:40 +0900

    youtubeID수정

--- js/youtube.js
+++ js/youtube.js
@@ -11,11 +11,11 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 function onYouTubePlayerAPIReady() {
   // <div id="player"></div>
   new YT.Player('player', {
-    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
+    videoId: 'nskYndiHaA8', // 최초 재생할 유튜브 영상 ID, 모차르트로 변경
     playerVars: {
       autoplay: true, // 자동 재생 유무
-      loop: true, // 반복 재생 유무
-      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
+      loop: false, // 반복 재생 유무
+      playlist: 'nskYndiHaA8' // 반복 재생할 유튜브 영상 ID 목록
     },
     events: {
       // 영상이 준비되었을 때,
